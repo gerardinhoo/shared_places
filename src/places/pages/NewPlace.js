85242e7ebe5070a04461cd55d8a2c015fb236981
Gerard Eklu <gerard_eklu@homedepot.com>
2020-04-25 19:17:40 -0400

    Managing state in the input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <h2>NewPlace Works!</h2>
+  return (
+    <div className="place-form">
+      <Input 
+        element="input" 
+        type="text" 
+        label="Title"
+        validators={[]}
+        errorText="Please Enter A Valid Title"
+      />
+    </div>
+  )
 };
 
 export default NewPlace;
