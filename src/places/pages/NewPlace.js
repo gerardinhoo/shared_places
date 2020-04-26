29aea7a0c249110dc51412b4dfe4d7902b5ddd3e
Gerard Eklu <gerard_eklu@homedepot.com>
2020-04-26 09:49:41 -0400

    Adding input validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Input from "../../shared/components/FormElements/Input";
+import {VALIDATOR_REQUIRE} from "../../shared/components/util/validators"
 import "./NewPlace.css";
 
 const NewPlace = () => {
@@ -9,7 +10,7 @@ const NewPlace = () => {
         element="input" 
         type="text" 
         label="Title"
-        validators={[]}
+        validators={[VALIDATOR_REQUIRE()]}
         errorText="Please Enter A Valid Title"
       />
     </div>
